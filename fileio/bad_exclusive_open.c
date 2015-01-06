ae952c68693f7c26cf5b508a5c615ce3b5f99f07
philomuzzi <gxlee1986@gmail.com>
2015-01-06 02:20:59 +0000

    fix bug

--- fileio/bad_exclusive_open.c
+++ fileio/bad_exclusive_open.c
@@ -24,7 +24,7 @@ int main(int argc, char** argv)
             }
 
             fd = open(argv[1], O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
-            if (fd = -1)
+            if (fd == -1)
                 errExit("open");
 
             printf("[PID %ld] Created file \"%s\" exclusively\n", (long)getpid(), argv[1]);
