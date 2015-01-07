50a37440191dd7effeb310f1f4bfb0e3fd3e51bd
philomuzzi <gxlee1986@gmail.com>
2015-01-07 10:53:21 +0800

    add atmoic_append.c

---
+++ fileio/atomic_append.c
@@ -0,0 +1,44 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+
+extern "C"
+{
+    #include "tlpi_hdr.h"
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 3 || strcmp(argv[1], "--help") == 0)
+        usageErr("%s file\n", argv[0]);
+
+    size_t num_bytes;
+    bool bAppend = true;
+    int openFlags = O_WRONLY | O_CREAT | O_APPEND;
+
+    num_bytes == getLong(&argv[2], GN_ANY_BASE, argv[2]);
+
+    if (argc == 4 && strcmp(argv[3], "x") == 0)
+    {
+        openFlags = O_WRONLY | O_CREAT;
+        bAppend = false;
+    }
+
+    int fd = open(argv[1], openFlags);
+    if (fd == -1)
+        errExit("open");
+
+    while(num_bytes--)
+    {
+        if (bAppend) {
+            if (write(fd, "1", 1) != 1)
+                Fatal("Partial/failed write");
+        } else {
+            if (lseek(fd, 0, SEEK_END) == -1)
+                errExit("lseek");
+            if (write(fd, "1", 1) != 1)
+                Fatal("Partial/failed write");
+        }
+    }
+
+    exit(EXIT_SUCCESS);
+}
