96e9d9b5fe2c1dfb4e1585871bf3c81a8e286593
philomuzzi <gxlee1986@gmail.com>
2015-01-16 03:07:57 +0000

    add test, show longjmp error

--- proc/test_longjmp.c
+++ proc/test_longjmp.c
@@ -8,15 +8,32 @@ extern "C" {
 
 static jmp_buf env;
 
-void static doJmp()
+void doJmp()
 {
     int i = 0;
+    char *y = "xyz";
     printf("in doJmp: %d\n", i);
+    printf("str y: %s\n", y);
     i = setjmp(env);
     if (i == 0)
+    {
         printf("after first setjmp: %d\n", i);
+        printf("str y: %s\n", y);
+    }
     else
+    {
         printf("after second setjmp: %d\n", i);
+        // error
+        printf("str y: %s\n", y);
+    }
+}
+
+void follow()
+{
+    int x = 1, y = 3, z = 5;
+    char *t = "qwert";
+    printf("follow x: %d, y: %d, z:%d\n", x, y, z);
+    printf("str t: %s\n", t);
 }
 
 int main(int argc, char const *argv[])
@@ -24,11 +41,14 @@ int main(int argc, char const *argv[])
     bool bJmp = true;
 
     doJmp();
+    follow();
 
     if(bJmp) {
         bJmp = false;
         longjmp(env, 1);
     }
+
+    printf("main end\n");
 
     return 0;
 }
