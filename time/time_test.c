db98cd025eaa4c9a3b0b6a7db1846553f776e35f
philomuzzi <gxlee1986@gmail.com>
2015-01-19 06:28:57 +0000

    fix bugs, add test

--- time/time_test.c
+++ time/time_test.c
@@ -8,17 +8,32 @@ extern "C" {
 int main(int argc, char **argv)
 {
     struct timeval tv;
-    if (0 == gettimeofday(&tv, NULL))
+    if (-1 == gettimeofday(&tv, NULL))
     {
         errExit("gettimeofday");
     }
 
-    printf("current time seconds: %d, usec: %ld\n", tv.tv_sec, tv.tv_usec);
+    printf("current time seconds: %ld, usec: %ld\n", tv.tv_sec, tv.tv_usec);
 
     time_t tt;
     tt = time(NULL);
 
-    printf("current time seconds: %d\n", tt);
+    printf("current time seconds: %ld\n", tt);
+
+    char * tchar = ctime(&tt);
+    if (!tchar)
+        errExit("ctime");
+
+    printf("current time: %s", tchar);
+
+    tchar = NULL;
+    char buf[100];
+    memset(buf, 0, 100);
+    tchar = ctime_r(&tt, buf);
+    if (!tchar)
+        errExit("ctime_r");
+
+    printf("current time: %s", buf);
 
     return 0;
 }
