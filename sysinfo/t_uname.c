1541481ecbd5dad7370c0577423cf9a2024e0ee7
philomuzzi <gxlee1986@gmail.com>
2015-01-19 17:28:57 +0800

    add files

---
+++ sysinfo/t_uname.c
@@ -0,0 +1,25 @@
+#define _GNU_SOURCE
+#include <sys/utsname.h>
+
+extern "C" {
+    #include "tlpi_hdr.h"
+}
+
+int main(int argc, char const *argv[])
+{
+    struct utsname uts;
+
+    if (uname(&uts) == -1)
+        errExit("uname");
+
+    printf("Node name:      %s\n", uts.nodename);
+    printf("System name:    %s\n", uts.sysname);
+    printf("Release:        %s\n", uts.release);
+    printf("Version:        %s\n", uts.version);
+    printf("Machine:        %s\n", uts.machine);
+#ifdef _GNU_SOURCE
+    printf("Domain name:    %s\n", uts.domainname);
+#endif
+
+    return 0;
+}
