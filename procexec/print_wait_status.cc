914c6d8c568a1f0cf90d3d70d7c39b52730abb55
philomuzzi <gxlee1986@gmail.com>
2015-01-27 16:06:32 +0800

    add proc examples

---
+++ procexec/print_wait_status.cc
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <sys/wait.h>
+#include "print_wait_status.h"
+
+extern "C" {
+    #include "tlpi_hdr.h"
+}
+
+void printWaitStatus(const char *msg, int status)
+{
+    if (msg != NULL)
+        printf("%s\n", msg);
+
+    if (WIFEXITED(status)) {
+        printf("child exited, status = %d\n", WEXITSTATUS(status));
+    } else if (WIFSIGNALED(status)) {
+        printf("child killed by signal %d (%s)\n", WTERMSIG(status), strsignal(WTERMSIG(status)));
+    #ifdef WCOREDUMP
+        if (WCOREDUMP(status))
+            printf(" (code dumped)");
+    #endif
+        printf("\n");
+    } else if (WIFSTOPPED(status)) {
+        printf("child stopped by signal %d (%s)\n", WSTOPSIG(status), strsignal(WSTOPSIG(status)));
+#ifdef WIFCONTINUED
+    } else if (WIFCONTINUED(status))
+        printf("child continued\n");
+#endif
+    } else {
+        printf("what happed to this child? (status=%x)\n", (unsigned int) status);
+    }
+}
